c6d04e6b036d9ee66243d4f9e5ed96edfa415ba2
StOnEOP <jonasm10a@gmail.com>
2022-01-19 22:43:01 +0000

    Name change to hermestravel

--- tables.sql
+++ tables.sql
@@ -8,19 +8,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema hermestravel
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema hermestravel
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `hermestravel` DEFAULT CHARACTER SET utf8 ;
+USE `hermestravel` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Utilizador`
+-- Table `hermestravel`.`Utilizador`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Utilizador` (
+CREATE TABLE IF NOT EXISTS `hermestravel`.`Utilizador` (
   `email` VARCHAR(25) NOT NULL,
   `nome` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
@@ -31,18 +31,18 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Rota`
+-- Table `hermestravel`.`Rota`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Rota` (
+CREATE TABLE IF NOT EXISTS `hermestravel`.`Rota` (
   `nome` VARCHAR(25) NOT NULL,
   PRIMARY KEY (`nome`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`PontoInteresse`
+-- Table `hermestravel`.`PontoInteresse`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`PontoInteresse` (
+CREATE TABLE IF NOT EXISTS `hermestravel`.`PontoInteresse` (
   `nome` VARCHAR(25) NOT NULL,
   `descricao` VARCHAR(45) NULL,
   `cidade` VARCHAR(45) NULL,
@@ -52,9 +52,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Rota_has_PontoInteresse`
+-- Table `hermestravel`.`Rota_has_PontoInteresse`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Rota_has_PontoInteresse` (
+CREATE TABLE IF NOT EXISTS `hermestravel`.`Rota_has_PontoInteresse` (
   `Rota_nome` VARCHAR(25) NOT NULL,
   `PontoInteresse_nome` VARCHAR(25) NOT NULL,
   PRIMARY KEY (`Rota_nome`, `PontoInteresse_nome`),
@@ -62,21 +62,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Rota_has_PontoInteresse` (
   INDEX `fk_Rota_has_PontoInteresse_Rota_idx` (`Rota_nome` ASC) VISIBLE,
   CONSTRAINT `fk_Rota_has_PontoInteresse_Rota`
     FOREIGN KEY (`Rota_nome`)
-    REFERENCES `mydb`.`Rota` (`nome`)
+    REFERENCES `hermestravel`.`Rota` (`nome`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Rota_has_PontoInteresse_PontoInteresse1`
     FOREIGN KEY (`PontoInteresse_nome`)
-    REFERENCES `mydb`.`PontoInteresse` (`nome`)
+    REFERENCES `hermestravel`.`PontoInteresse` (`nome`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Avaliacao`
+-- Table `hermestravel`.`Avaliacao`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Avaliacao` (
+CREATE TABLE IF NOT EXISTS `hermestravel`.`Avaliacao` (
   `idAvaliacao` INT NOT NULL,
   `avaliacao` INT NULL,
   `descricao` VARCHAR(45) NULL,
@@ -87,21 +87,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Avaliacao` (
   INDEX `fk_Avaliacao_Utilizador1_idx` (`Utilizador_email` ASC) VISIBLE,
   CONSTRAINT `fk_Avaliacao_PontoInteresse1`
     FOREIGN KEY (`PontoInteresse_nome`)
-    REFERENCES `mydb`.`PontoInteresse` (`nome`)
+    REFERENCES `hermestravel`.`PontoInteresse` (`nome`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Avaliacao_Utilizador1`
     FOREIGN KEY (`Utilizador_email`)
-    REFERENCES `mydb`.`Utilizador` (`email`)
+    REFERENCES `hermestravel`.`Utilizador` (`email`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Categoria`
+-- Table `hermestravel`.`Categoria`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Categoria` (
+CREATE TABLE IF NOT EXISTS `hermestravel`.`Categoria` (
   `idCategoria` INT NOT NULL,
   `Descricao` VARCHAR(45) NULL,
   PRIMARY KEY (`idCategoria`))
@@ -109,9 +109,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`PontoInteresse_has_Categoria`
+-- Table `hermestravel`.`PontoInteresse_has_Categoria`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`PontoInteresse_has_Categoria` (
+CREATE TABLE IF NOT EXISTS `hermestravel`.`PontoInteresse_has_Categoria` (
   `PontoInteresse_nome` VARCHAR(25) NOT NULL,
   `Categoria_idCategoria` INT NOT NULL,
   PRIMARY KEY (`PontoInteresse_nome`, `Categoria_idCategoria`),
@@ -119,21 +119,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`PontoInteresse_has_Categoria` (
   INDEX `fk_PontoInteresse_has_Categoria_PontoInteresse1_idx` (`PontoInteresse_nome` ASC) VISIBLE,
   CONSTRAINT `fk_PontoInteresse_has_Categoria_PontoInteresse1`
     FOREIGN KEY (`PontoInteresse_nome`)
-    REFERENCES `mydb`.`PontoInteresse` (`nome`)
+    REFERENCES `hermestravel`.`PontoInteresse` (`nome`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_PontoInteresse_has_Categoria_Categoria1`
     FOREIGN KEY (`Categoria_idCategoria`)
-    REFERENCES `mydb`.`Categoria` (`idCategoria`)
+    REFERENCES `hermestravel`.`Categoria` (`idCategoria`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Utilizador_hasfavorite_PontoInteresse`
+-- Table `hermestravel`.`Utilizador_hasfavorite_PontoInteresse`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Utilizador_hasfavorite_PontoInteresse` (
+CREATE TABLE IF NOT EXISTS `hermestravel`.`Utilizador_hasfavorite_PontoInteresse` (
   `Utilizador_email` VARCHAR(25) NOT NULL,
   `PontoInteresse_nome` VARCHAR(25) NOT NULL,
   PRIMARY KEY (`Utilizador_email`, `PontoInteresse_nome`),
@@ -141,21 +141,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Utilizador_hasfavorite_PontoInteresse` (
   INDEX `fk_Utilizador_has_PontoInteresse_Utilizador1_idx` (`Utilizador_email` ASC) VISIBLE,
   CONSTRAINT `fk_Utilizador_has_PontoInteresse_Utilizador1`
     FOREIGN KEY (`Utilizador_email`)
-    REFERENCES `mydb`.`Utilizador` (`email`)
+    REFERENCES `hermestravel`.`Utilizador` (`email`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Utilizador_has_PontoInteresse_PontoInteresse1`
     FOREIGN KEY (`PontoInteresse_nome`)
-    REFERENCES `mydb`.`PontoInteresse` (`nome`)
+    REFERENCES `hermestravel`.`PontoInteresse` (`nome`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Utilizador_hashistorico_Rota`
+-- Table `hermestravel`.`Utilizador_hashistorico_Rota`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Utilizador_hashistorico_Rota` (
+CREATE TABLE IF NOT EXISTS `hermestravel`.`Utilizador_hashistorico_Rota` (
   `Utilizador_email` VARCHAR(25) NOT NULL,
   `Rota_nome` VARCHAR(25) NOT NULL,
   PRIMARY KEY (`Utilizador_email`, `Rota_nome`),
@@ -163,21 +163,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Utilizador_hashistorico_Rota` (
   INDEX `fk_Utilizador_has_Rota_Utilizador1_idx` (`Utilizador_email` ASC) VISIBLE,
   CONSTRAINT `fk_Utilizador_has_Rota_Utilizador1`
     FOREIGN KEY (`Utilizador_email`)
-    REFERENCES `mydb`.`Utilizador` (`email`)
+    REFERENCES `hermestravel`.`Utilizador` (`email`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Utilizador_has_Rota_Rota1`
     FOREIGN KEY (`Rota_nome`)
-    REFERENCES `mydb`.`Rota` (`nome`)
+    REFERENCES `hermestravel`.`Rota` (`nome`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Utilizador_has_Utilizador`
+-- Table `hermestravel`.`Utilizador_has_Utilizador`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Utilizador_has_Utilizador` (
+CREATE TABLE IF NOT EXISTS `hermestravel`.`Utilizador_has_Utilizador` (
   `Utilizador_email` VARCHAR(25) NOT NULL,
   `Utilizador_email1` VARCHAR(25) NOT NULL,
   PRIMARY KEY (`Utilizador_email`, `Utilizador_email1`),
@@ -185,12 +185,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Utilizador_has_Utilizador` (
   INDEX `fk_Utilizador_has_Utilizador_Utilizador1_idx` (`Utilizador_email` ASC) VISIBLE,
   CONSTRAINT `fk_Utilizador_has_Utilizador_Utilizador1`
     FOREIGN KEY (`Utilizador_email`)
-    REFERENCES `mydb`.`Utilizador` (`email`)
+    REFERENCES `hermestravel`.`Utilizador` (`email`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Utilizador_has_Utilizador_Utilizador2`
     FOREIGN KEY (`Utilizador_email1`)
-    REFERENCES `mydb`.`Utilizador` (`email`)
+    REFERENCES `hermestravel`.`Utilizador` (`email`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
