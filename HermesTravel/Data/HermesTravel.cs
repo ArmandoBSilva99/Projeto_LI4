841dd9884ddea3c2a6fbdc662f7500e2c3b29a70
StOnEOP <jonasm10a@gmail.com>
2022-01-22 19:03:27 +0000

    Api

--- HermesTravel/Data/HermesTravel.cs
+++ HermesTravel/Data/HermesTravel.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Device.Location;
 
 namespace HermesTravel.Data
 {
@@ -35,27 +34,6 @@ namespace HermesTravel.Data
         public List<string> getCities()
         {
             return modelInterestPoints.getCities();
-        }
-
-        void GetLocationProperty()
-        {
-            GeoCoordinateWatcher watcher = new GeoCoordinateWatcher();
-
-            // Do not suppress prompt, and wait 1000 milliseconds to start.
-            watcher.TryStart(false, TimeSpan.FromMilliseconds(1000));
-
-            GeoCoordinate coord = watcher.Position.Location;
-
-            if (coord.IsUnknown != true)
-            {
-                Console.WriteLine("Lat: {0}, Long: {1}",
-                    coord.Latitude,
-                    coord.Longitude);
-            }
-            else
-            {
-                Console.WriteLine("Unknown latitude and longitude.");
-            }
         }
     }
 }
